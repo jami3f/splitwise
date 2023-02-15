3d47f73b8ed5ff2719e588f7f98aedab87310fed
jami3f <jamie.fairhurst01@gmail.com>
2023-02-15 16:44:56 +0000

    Rearranged items

--- app/src/InputField.tsx
+++ app/src/InputField.tsx
@@ -9,48 +9,57 @@ import { RefObject } from "preact";
 import { Person } from "./app";
 import remove from "./assets/cancel.svg";
 
+function ItemsDisplay(props: {
+  items: number[];
+  removeItem: (index: number) => void;
+}) {
+  return (
+    <p className="col-span-2">
+      {props.items.map((item, index) => (
+        <div className="inline-flex align-middle mr-3">
+          <span className="mr-1">£{item.toFixed(2)}</span>
+          <button
+            title="Remove"
+            type="button"
+            onClick={() => props.removeItem(index)}
+            tabIndex={-1}
+          >
+            <img src={remove} alt="remove" className="w-3" />
+          </button>
+        </div>
+      ))}
+    </p>
+  );
+}
+
 export function InputSection(props: {
   people: Person[];
   handleNameClick: (event: Event) => void;
   className?: string;
-  number: number;
+  index: number;
 }) {
+  const [shouldFocus, setShouldFocus] = useState(false);
   const [items, updateItems] = useState<number[]>([]);
   const ref = useRef(null) as RefObject<HTMLInputElement>;
-  let selected = false;
 
   function addItem(item: number) {
     updateItems((old) => [...old, item]);
   }
+
   function removeItem(index: number) {
     updateItems((old) => old.filter((_, i) => i !== index));
   }
 
+  // Refocus input after adding an item
   useEffect(() => {
-    if (selected) {
-      ref.current?.focus();
-      selected = false;
-    }
-  }, [InputSection]);
+    ref.current?.focus();
+    setShouldFocus(false);
+  }, [shouldFocus]);
 
-  function ItemsDisplay() {
-    return (
-      <p className="col-span-2">
-        {items.map((item, index) => (
-          <div className="inline-flex align-middle mr-3">
-            <span className="mr-1">£{item.toFixed(2)}</span>
-            <button
-              title="Remove"
-              type="button"
-              onClick={() => removeItem(index)}
-            >
-              <img src={remove} alt="remove" className="w-3" />
-            </button>
-          </div>
-        ))}
-      </p>
-    );
-  }
+  // No input focussed by default
+  useEffect(() => {
+    ref.current?.blur();
+  }, []);
 
   return (
     <div className={"p-2 grid grid-cols-6 gap-x-2 border"}>
@@ -66,18 +75,19 @@ export function InputSection(props: {
         key={props.people}
         className="border self-center"
         onChange={(e: any) => {
+          setShouldFocus(true);
           const newValue = parseFloat(e.target.value);
-          if (isNaN(newValue))
+          if (isNaN(newValue) || newValue <= 0) {
             return ((e.target as HTMLInputElement).value = "");
+          }
           addItem(newValue);
           props.people.forEach((p) =>
             p.addItem(newValue / props.people.length)
           );
           e.target.value = "";
-          selected = true;
         }}
       ></input>
-      <ItemsDisplay />
+      <ItemsDisplay items={items} removeItem={removeItem} />
       <p className="col-span-2 inline">
         {props.people.map((p, _, arr) => {
           const prefix = arr.length > 1 ? p.name + ": " : "";
--- app/src/app.tsx
+++ app/src/app.tsx
@@ -26,6 +26,92 @@ export class Person {
   }
 }
 
+function TopBar() {
+  return (
+    <div className="h-5 flex bg-gradient-to-r from-red-300 to-blue-300 w-200% animate-inf-scroll"></div>
+  );
+}
+
+function SelectPrompt(props: {
+  selection: Person[];
+  setSelection: StateUpdater<Person[]>;
+  setSelectionMode: StateUpdater<boolean>;
+  shared: Person[][];
+  setShared: StateUpdater<Person[][]>;
+}) {
+  const checkExists = () => {
+    if (props.selection.length === 1) return true;
+    for (const personArr of props.shared) {
+      if (personArr.length !== props.selection.length) continue;
+      if (
+        personArr.every((person) =>
+          props.selection.map((p) => p.name).includes(person.name)
+        )
+      )
+        return true;
+    }
+  };
+
+  return (
+    <div className="bg-gray-100 p-2">
+      <p className="text-gray-500">Select people to share an item:</p>
+      {props.selection.map((person) => (
+        <p className={checkExists() ? "text-red-600" : ""}>{person.name}</p>
+      ))}
+      <button
+        title="done"
+        type="button"
+        onClick={() => {
+          if (checkExists()) return;
+          props.setShared((old) => [...old, props.selection]);
+          props.setSelection([]);
+          props.setSelectionMode(false);
+        }}
+      >
+        <img src={done} alt="" />
+      </button>
+      <button
+        title="cancel"
+        type="button"
+        onClick={() => {
+          props.setSelection([]);
+          props.setSelectionMode(false);
+        }}
+      >
+        <img src={cancel} alt="" />
+      </button>
+    </div>
+  );
+}
+
+function AddSharedButton(props: {
+  selection: Person[];
+  setSelection: StateUpdater<Person[]>;
+  selectionMode: boolean;
+  setSelectionMode: StateUpdater<boolean>;
+  shared: Person[][];
+  setShared: StateUpdater<Person[][]>;
+}) {
+  return props.selectionMode ? (
+    <SelectPrompt
+      selection={props.selection}
+      setSelection={props.setSelection}
+      setSelectionMode={props.setSelectionMode}
+      shared={props.shared}
+      setShared={props.setShared}
+    />
+  ) : (
+    <button
+      className=" bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold text-sm m-2 py-2 px-2 rounded inline-flex items-center transition-colors"
+      type="button"
+      onClick={() => props.setSelectionMode(true)}
+    >
+      <img alt="add icon" src={add} className="w-5 mr-1"></img>
+      Add Shared Item
+    </button>
+  );
+}
+
 export function App() {
   function handleNameClick(event: Event) {
     const name = (event.target as HTMLParagraphElement).innerText;
@@ -35,12 +121,6 @@ export function App() {
       return;
     }
     setSelection((old) => [...old, people[name]]);
-  }
-
-  function TopBar() {
-    return (
-      <div className="h-5 flex bg-gradient-to-r from-red-300 to-blue-300 w-200% animate-inf-scroll"></div>
-    );
   }
 
   const people: { [key: string]: Person } = {
@@ -54,52 +134,6 @@ export function App() {
   const [selectionMode, setSelectionMode] = useState(false);
   const [selection, setSelection] = useState<Person[]>([]);
 
-  function SelectPrompt() {
-    const checkExists = () => {
-      if (selection.length === 1) return true;
-      for (const personArr of shared) {
-        if (personArr.length !== selection.length) continue;
-        if (
-          personArr.every((person) =>
-            selection.map((p) => p.name).includes(person.name)
-          )
-        )
-          return true;
-      }
-    };
-
-    return (
-      <div className="bg-gray-100 p-2">
-        <p className="text-gray-500">Select people to share an item:</p>
-        {selection.map((person) => (
-          <p className={checkExists() ? "text-red-600" : ""}>{person.name}</p>
-        ))}
-        <button
-          title="done"
-          type="button"
-          onClick={() => {
-            if (checkExists()) return;
-            setShared((old) => [...old, selection]);
-            setSelection([]);
-            setSelectionMode(false);
-          }}
-        >
-          <img src={done} alt="" />
-        </button>
-        <button
-          title="cancel"
-          type="button"
-          onClick={() => {
-            setSelection([]);
-            setSelectionMode(false);
-          }}
-        >
-          <img src={cancel} alt="" />
-        </button>
-      </div>
-    );
-  }
-
   function Headings() {
     return (
       <div className="grid grid-cols-6 p-2 ">
@@ -111,68 +145,46 @@ export function App() {
     );
   }
 
-  function IndividualsList() {
-    return (
-      <>
-        {Object.values(people).map((person) => (
-          <InputSection
-            className={selectionMode ? "text-blue-600 cursor-pointer" : ""}
-            people={[person]}
-            number={1}
-            handleNameClick={handleNameClick}
-          />
-        ))}
-      </>
-    );
-  }
-
-  function SharedList() {
-    return (
-      <>
-        {shared.map((people) => (
-          <InputSection
-            className="text-gray-700"
-            people={[...people]}
-            number={people.length}
-            handleNameClick={() => {}}
-          ></InputSection>
-        ))}
-      </>
-    );
-  }
-
-  function AddSharedButton() {
-    return selectionMode ? (
-      <SelectPrompt />
-    ) : (
-      <button
-        className=" bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold text-sm m-2 py-2 px-2 rounded inline-flex items-center transition-colors"
-        type="button"
-        onClick={() => setSelectionMode(true)}
-      >
-        <img alt="add icon" src={add} className="w-5 mr-1"></img>
-        Add Shared Item
-      </button>
-    );
-  }
+  let keyCounter = 1;
 
   return (
     <div className="w-screen overflow-hidden">
       <TopBar />
-
       <div className="grid grid-cols-1">
         <div className="border w-auto h-auto">
           <Headings />
-          <IndividualsList />
+          {Object.values(people).map((person) => (
+            <InputSection
+              className={selectionMode ? "text-blue-600 cursor-pointer" : ""}
+              people={[person]}
+              index={keyCounter++}
+              handleNameClick={handleNameClick}
+            />
+          ))}
           <InputSection
             className="text-orange-500"
             people={[new Person("Service")]}
-            number={1}
+            index={keyCounter++}
             handleNameClick={() => {}}
           />
-          <SharedList />
-
-          <AddSharedButton />
+          {shared.map((people) => (
+            <InputSection
+              className="text-gray-700"
+              people={[...people]}
+              index={keyCounter++}
+              handleNameClick={() => {}}
+            ></InputSection>
+          ))}
+          <AddSharedButton
+            {...{
+              selection,
+              setSelection,
+              selectionMode,
+              setSelectionMode,
+              shared,
+              setShared,
+            }}
+          />
         </div>
       </div>
     </div>
