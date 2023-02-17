fcd0aacac0b86756bbb580acfe730a5d53b596a0
jami3f <jamie.fairhurst01@gmail.com>
2023-02-17 22:38:34 +0000

    Started promotion implementation

--- app/src/InputField.tsx
+++ app/src/InputField.tsx
@@ -5,24 +5,36 @@ import {
   StateUpdater,
   EffectCallback,
 } from "preact/hooks";
-import { RefObject } from "preact";
+import { JSX, RefObject } from "preact";
 import { Person } from "./app";
 import remove from "./assets/cancel.svg";
 
-interface SharedItem {
+import { PromotionTotal } from "./components/PromotionTotal";
+
+export interface SharedItem {
   ids: number[];
   price: number;
 }
 
-function ItemsDisplay(props: {
+export enum DisplayType {
+  Price,
+  Percent,
+}
+
+export function ItemsDisplay(props: {
   items: SharedItem[];
   removeItem: (ids: number[]) => void;
+  type: DisplayType;
 }) {
   return (
     <p className="col-span-2">
       {props.items.map((item, index) => (
         <div className="inline-flex align-middle mr-3">
-          <span className="mr-1">£{item.price.toFixed(2)}</span>
+          <span className="mr-1">
+            {props.type === DisplayType.Price && "£"}
+            {item.price.toFixed(2).toString()}
+            {props.type === DisplayType.Percent && "%"}
+          </span>
           <button
             title="Remove"
             type="button"
@@ -37,31 +49,32 @@ function ItemsDisplay(props: {
   );
 }
 
-function TotalDisplay(props:{people: Person[], items: SharedItem[]})
-{
-  return(<p className="col-span-2 inline">
-  {props.people.map((p, _, arr) => {
-    const prefix = arr.length > 1 ? p.name + ": " : "";
-    return (
-      <p>
-        {props.items.length > 0
-          ? `${prefix}£${(
-              props.items.map((i) => i.price).reduce((a, b) => a + b) /
-              props.people.length
-            ).toFixed(2)}`
-          : ""}
-      </p>
-    );
-  })}
-</p>)
+function TotalDisplay(props: { people: Person[]; items: SharedItem[] }) {
+  return (
+    <p className="col-span-2 inline">
+      {props.people.map((p, _, arr) => {
+        const prefix = arr.length > 1 ? p.name + ": " : "";
+        return (
+          <p>
+            {props.items.length > 0
+              ? `${prefix}£${(
+                  props.items.map((i) => i.price).reduce((a, b) => a + b) /
+                  props.people.length
+                ).toFixed(2)}`
+              : ""}
+          </p>
+        );
+      })}
+    </p>
+  );
 }
 
 export function InputSection(props: {
   people: Person[];
   handleNameClick: (event: Event) => void;
-  index: number;
   className?: string;
   name?: string;
+  limit?: number;
 }) {
   const [shouldFocus, setShouldFocus] = useState(false);
   const [items, updateItems] = useState<SharedItem[]>([]);
@@ -74,13 +87,11 @@ export function InputSection(props: {
       p.addItem({ id: p.getID(), price: price / props.people.length })
     );
   }
-
-  const compArrays = (a: number[], b: number[]) => {
-    if (a.length !== b.length) return false;
-    return a.every((v, i) => v === b[i]);
-  };
-
   function removeItem(ids: number[]) {
+    const compArrays = (a: number[], b: number[]) => {
+      if (a.length !== b.length) return false;
+      return a.every((v, i) => v === b[i]);
+    };
     updateItems((old) => old.filter((i) => !compArrays(i.ids, ids)));
     props.people.forEach((p, index) => p.removeItem(ids[index]));
   }
@@ -96,31 +107,45 @@ export function InputSection(props: {
     ref.current?.blur();
   }, []);
 
+  const promotion = props.name === "Promotion";
+
   return (
-    <div className={"p-2 grid grid-cols-6 gap-x-2 border"}>
+    <div className={"p-2 grid grid-cols-7 gap-x-2 border-b"}>
       <p
         onClick={props.handleNameClick}
-        className={"self-center w-1/4 " + props.className}
+        className={"self-center w-1/4 col-span-2" + " " + props.className}
       >
         {props.name || props.people.map((p) => p.name).join("\n")}
       </p>
       <input
         ref={ref}
-        title={props.people.map((p) => p.name).join("\n")}
+        title={props.name || props.people.map((p) => p.name).join("\n")}
         key={props.people}
         className="border self-center"
         onChange={(e: any) => {
           setShouldFocus(true);
           const newValue = parseFloat(e.target.value);
-          if (isNaN(newValue) || newValue <= 0) {
+          if (
+            isNaN(newValue) ||
+            newValue <= 0 ||
+            (props.limit && items.length >= props.limit)
+          ) {
             return ((e.target as HTMLInputElement).value = "");
           }
           addItem(newValue);
           e.target.value = "";
         }}
       ></input>
-      <ItemsDisplay items={items} removeItem={removeItem} />
-      <TotalDisplay people={props.people} items={items} />
+      <ItemsDisplay
+        items={items}
+        removeItem={removeItem}
+        type={promotion ? DisplayType.Percent : DisplayType.Price}
+      />
+      {promotion ? (
+        <PromotionTotal people={props.people} />
+      ) : (
+        <TotalDisplay people={props.people} items={items} />
+      )}
     </div>
   );
 }
---
+++ app/src/components/PromotionTotal.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "preact/hooks";
+import { Person } from "../app";
+import { SharedItem } from "../InputField";
+
+export function PromotionTotal(props: { people: Person[] }) {
+  if (!props.people[0].items[0]) return <p></p>;
+  const promotionValue = props.people[0].items[0].price;
+  const maxPromotion = props.people[1].items[0].price;
+  useEffect(() => {
+    console.log(promotionValue)
+  }, [promotionValue])
+  return (
+    <p className="col-span-2 inline">
+      {props.people.slice(2).map((p) => {
+        return (
+          <p>{p.name + ": " + "£" + (p.total * (promotionValue/100)).toFixed(2)}</p>
+        );
+      })}
+    </p>
+  );
+}
