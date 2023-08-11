44ea47ca0939075c521f11b77b82a9bc82fe8e02
jami3f <jamie.fairhurst01@gmail.com>
2023-08-11 15:50:14 +0100

    removed forwardref from inputfield and fixed materialui

--- src/Components/Common/InputField.tsx
+++ src/Components/Common/InputField.tsx
@@ -11,71 +11,67 @@ import { inputErrorKeyframes } from "../../assets/Keyframes";
 
 const ENTER_KEYCODE = 13;
 
-const InputField = forwardRef(
-  (
-    props: {
-      name: string;
-      handleInput: (e: any) => void;
-      addToRefObject?: (ref: HTMLInputElement) => void;
-      handleEmpty?: () => void;
-      numeric?: boolean;
-      errorCondition?: (e: any) => boolean;
-      className?: string;
-    },
-    ref: ForwardedRef<HTMLInputElement>
+function InputField(props: {
+  name: string;
+  handleInput: (e: any) => void;
+  addToRefObject?: (ref: HTMLInputElement) => void;
+  handleEmpty?: () => void;
+  numeric?: boolean;
+  errorCondition?: (e: any) => boolean;
+  className?: string;
+}) {
+  const parseInput = (
+    e: any,
+    handleInput: (e: any) => void,
+    handleEmpty?: () => void,
+    errorCondition?: (e: any) => boolean
   ) => {
-    const parseInput = (
-      e: any,
-      handleInput: (e: any) => void,
-      handleEmpty?: () => void,
-      errorCondition?: (e: any) => boolean
-    ) => {
-      if (e.target.value === "") return handleEmpty?.();
-      if (errorCondition && errorCondition(e)) {
-        inputErrorAnimation.start(inputErrorKeyframes);
-        return ((e.target as HTMLInputElement).value = "");
+    if (e.target.value === "") return handleEmpty?.();
+    if (errorCondition && errorCondition(e)) {
+      inputErrorAnimation.start(inputErrorKeyframes);
+      return ((e.target as HTMLInputElement).value = "");
+    }
+    handleInput(e);
+  };
+
+  const inputErrorAnimation = useAnimationControls();
+
+  const localRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    props.addToRefObject?.(localRef.current as HTMLInputElement);
+  }, []);
+
+  return (
+    <motion.input
+      ref={localRef}
+      key={props.name}
+      title={props.name}
+      // animate={inputErrorAnimation}
+      // transition={{ duration: 0.5 }}
+      className={`border self-center rounded ${props.className}`}
+      onKeyDown={(e: any) =>
+        e.keyCode === ENTER_KEYCODE &&
+        parseInput(
+          e,
+          props.handleInput,
+          props.handleEmpty,
+          props.errorCondition
+        )
       }
-      handleInput(e);
-    };
-
-    const inputErrorAnimation = useAnimationControls();
-
-    const localRef = useRef<HTMLInputElement>(null);
-
-    useEffect(() => {
-      props.addToRefObject?.(localRef.current as HTMLInputElement);
-    }, []);
-
-    return (
-      <motion.input
-        ref={localRef}
-        title={props.name}
-        animate={inputErrorAnimation}
-        transition={{ duration: 0.5 }}
-        className={`border self-center rounded ${props.className}`}
-        onKeyDown={(e: any) =>
-          e.keyCode === ENTER_KEYCODE &&
-          parseInput(
-            e,
-            props.handleInput,
-            props.handleEmpty,
-            props.errorCondition
-          )
-        }
-        onBlur={(e: any) =>
-          parseInput(
-            e,
-            props.handleInput,
-            props.handleEmpty,
-            props.errorCondition
-          )
-        }
-        {...(props.numeric
-          ? { type: "number", inputMode: "decimal" }
-          : { type: "text" })}
-      ></motion.input>
-    );
-  }
-);
+      onBlur={(e: any) =>
+        parseInput(
+          e,
+          props.handleInput,
+          props.handleEmpty,
+          props.errorCondition
+        )
+      }
+      {...(props.numeric
+        ? { type: "number", inputMode: "decimal" }
+        : { type: "text" })}
+    ></motion.input>
+  );
+}
 
 export default InputField;
