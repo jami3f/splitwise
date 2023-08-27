9c123e450084819f870b11d0c999e9190a01d2ba
jami3f <jamie.fairhurst01@gmail.com>
2023-08-27 19:11:39 +0100

    Added remainder logic

--- src/Components/Totals/TotalView.tsx
+++ src/Components/Totals/TotalView.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import { Person } from "../../Types";
 
 export default function TotalView(props: {
@@ -11,6 +11,10 @@ export default function TotalView(props: {
 
   const grandTotal = peopleFiltered.reduce((acc, p) => acc + p.total, 0);
 
+  const [remainder, setRemainder] = useState(0);
+
+  const [totals, setTotals] = useState<{ name: string; total: number }[]>([]);
+
   const calculateTotalWithPromotion = (total: number) => {
     if (
       (props.promotionCap || 0) === 0 ||
@@ -21,12 +25,24 @@ export default function TotalView(props: {
     // hits max promotion and calculates how much to take off each person's order - weighted by how much they spent
     else return total - (total / grandTotal) * (props.promotionCap || 0);
   };
-  const totals = peopleFiltered.map((person) => {
-    const totalNoService = calculateTotalWithPromotion(person.total);
-    const individualTotal =
-      totalNoService + props.service / peopleFiltered.length;
-    return { name: person.name, total: individualTotal };
-  });
+
+  useEffect(() => {
+    const tempTotals = peopleFiltered.map((person) => {
+      const totalNoService = calculateTotalWithPromotion(person.total);
+      const individualTotal =
+        totalNoService + props.service / peopleFiltered.length;
+      return { name: person.name, total: individualTotal };
+    });
+    setTotals(tempTotals);
+    setRemainder(() => {
+      const total = totals.reduce((acc, p) => acc + p.total, 0);
+      const totalFixed = totals.reduce(
+        (acc, p) => acc + Math.floor(p.total * 10) / 10,
+        0
+      );
+      return total - totalFixed;
+    });
+  }, [peopleFiltered]);
 
   return (
     <div className="p-2 pr-5">
